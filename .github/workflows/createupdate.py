b658301c2afd715076f9b4dbd470b75cbfaa1a47
WillItUsername <johan.hagelskjaer.sjursen.01@regionh.dk>
2023-11-02 14:30:07 +0100

    createupdate and delete scripts

--- .github/workflows/createupdate.py
+++ .github/workflows/createupdate.py
@@ -1,8 +1,34 @@
 import sys
+import re
 
-with open(sys.argv[1]) as view:
-    view_str = view.read()
-    if "CREATEORREPLACEVIEW" in view_str.upper().replace(' ', ''):
-        pass
-    else:
-        raise Exception("Cannot find CREATE OR REPLACE VIEW in file.")
+def check_format_and_prepare_notebook(notebook_path, env):
+    """ Takes a notebook path and an environment letter.
+    Prepares the notebook to be executed against gold_dataenheden schema in the environment
+    """
+    with open(notebook_path) as notebook:
+        notebook_str = notebook.read()
+
+    # Search for valid create schema statments
+    if (matches := list(re.finditer(r"CREATE *SCHEMA *IF *NOT *EXISTS *dap_._gold_dataenheden_sandbox\.(.*);",notebook_str))):
+        if len(matches) < 1:
+            raise Exception('Did not find a CREATE SCHEMA IF NOT EXISTS statement.')
+        elif len(matches) > 1:
+            raise Exception('Found multiple CREATE SCHEMA IF NOT EXISTS statements.')
+        else:
+            match = matches[0]
+
+    if env not in ['d', 't', 'p']:
+        raise Exception('Invalid environment argument.')
+        
+
+    drop_create = f"DROP SCHEMA IF EXISTS dap_d_gold_dataenheden.{match.group(1)};\nCREATE SCHEMA IF NOT EXISTS dap_d_gold_dataenheden.{match.group(1)};"
+    temp_view_str = notebook_str[0:match.span()[0]] + drop_create + notebook_str[match.span()[1]:]
+    
+    temp_view_str = re.sub(r"dap_[dtp]_", f"dap_{env}_", temp_view_str)
+
+    with open(notebook_path.replace('.sql', '_temp.sql'), 'w') as temp_notebook:
+        temp_notebook.write(temp_view_str)
+
+if __name__ == '__main__':
+    check_format_and_prepare_notebook(sys.argv[1], sys.argv[2])
+
---
+++ .github/workflows/delete.py
@@ -0,0 +1,32 @@
+import sys
+import re
+
+def drop_schema(notebook_path, env):
+    """ Takes a notebook path and an environment letter.
+    Prepares the notebook to be executed against gold_dataenheden schema in the environment
+    """
+    with open(notebook_path) as notebook:
+        notebook_str = notebook.read()
+
+    # Search for valid create schema statments
+    if (matches := list(re.finditer(r"CREATE *SCHEMA *IF *NOT *EXISTS *dap_._gold_dataenheden_sandbox\.(.*);",notebook_str))):
+        if len(matches) < 1:
+            raise Exception('Did not find a CREATE SCHEMA IF NOT EXISTS statement.')
+        elif len(matches) > 1:
+            raise Exception('Found multiple CREATE SCHEMA IF NOT EXISTS statements.')
+        else:
+            match = matches[0]
+
+    if env not in ['d', 't', 'p']:
+        raise Exception('Invalid environment argument.')
+        
+
+    drop = f"DROP SCHEMA IF EXISTS dap_{env}_gold_dataenheden.{match.group(1)};"
+    temp_view_str = notebook_str[0:match.span()[0]] + drop
+
+    with open(notebook_path.replace('.sql', '_temp.sql'), 'w') as temp_notebook:
+        temp_notebook.write(temp_view_str)
+
+if __name__ == '__main__':
+    drop_schema(sys.argv[1], sys.argv[2])
+
